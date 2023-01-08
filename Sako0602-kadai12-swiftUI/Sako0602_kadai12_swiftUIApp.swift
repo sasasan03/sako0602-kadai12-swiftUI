f887419d906331d2732d6d705843e2dd5da216b0
sasasan03 <sako.hiro62@gmail.com>
2023-01-08 21:22:55 +0900

    Initial Commit

---
+++ Sako0602-kadai12-swiftUI/Sako0602_kadai12_swiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sako0602_kadai12_swiftUIApp.swift
+//  Sako0602-kadai12-swiftUI
+//
+//  Created by sako0602 on 2023/01/08.
+//
+
+import SwiftUI
+
+@main
+struct Sako0602_kadai12_swiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
